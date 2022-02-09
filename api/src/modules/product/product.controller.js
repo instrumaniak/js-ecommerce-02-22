a3da747d5194ec80b44d83e1e511e62113e28e09
Raziur Rahman <instrumaniak@gmail.com>
2022-02-09 22:31:32 +0600

    api: get all products

--- api/src/modules/product/product.controller.js
+++ api/src/modules/product/product.controller.js
@@ -1,5 +1,16 @@
-exports.getAllProducts = (req, res) => {
-  res.json({
-    msg: "all products",
-  });
+const ProductModel = require("./product.model");
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await ProductModel.find({});
+    return res.json({
+      success: true,
+      products,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
