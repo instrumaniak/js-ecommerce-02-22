2aa00bf30aa9ea8b58c6b72714e4f1c70df6dbd9
Raziur Rahman <instrumaniak@gmail.com>
2022-02-10 11:44:10 +0600

    api: added user registration

--- api/src/modules/user/user.controller.js
+++ api/src/modules/user/user.controller.js
@@ -1,6 +1,40 @@
-exports.register = (req, res) => {
-  res.json({
-    msg: "register",
+const bcrypt = require("bcryptjs");
+const UserModel = require("./user.model");
+
+exports.register = (req, res, next) => {
+  const { name, email, phone, password } = req.body;
+
+  const newUser = new UserModel({
+    name,
+    email,
+    phone,
+    password,
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) return next(err);
+      newUser.password = hash;
+      newUser.save((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            success: false,
+            message: "Failed to create new user.",
+            errors: err.errors,
+          });
+        }
+        return res.status(201).json({
+          success: true,
+          message: "User created successfully",
+          data: {
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            userId: data._id,
+          },
+        });
+      });
+    });
   });
 };
 
