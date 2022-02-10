bdc5e23b755842411fc379dcb1ca515fb48c348b
Raziur Rahman <instrumaniak@gmail.com>
2022-02-10 11:20:29 +0600

    api: json & common error handling

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,22 @@ mongoose
 const app = express();
 const PORT = 5000;
 
+app.use(express.json())
+
 app.use("/api/users", userRouter).use("/api/products", productRouter);
+
+app.use((req, res, next) => {
+  const err = new Error("path not found")
+  err.status = 404
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    errors: [err.message],
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}`);
