7c12706f7c7bf05befb45aa5e8a6665e2ca88948
Raziur Rahman <instrumaniak@gmail.com>
2022-02-11 21:25:54 +0600

    be: fix seeded products imageurl

--- api/src/db/seeder.js
+++ api/src/db/seeder.js
@@ -7,52 +7,52 @@ const productsData = [
   {
     name: "Wireless Headphone",
     description: "Standard wireless headphone",
-    imageUrl: "/assets/products01-headphone.jpg",
+    imageUrl: "/products/01-headphone.jpg",
   },
   {
     name: "Sunglass",
     description: "Stylist minimal sunglass",
-    imageUrl: "/assets/products03-sunglass.jpg",
+    imageUrl: "/products/03-sunglass.jpg",
   },
   {
     name: "Headphone",
     description: "Standard wired headphone",
-    imageUrl: "/assets/products05-headphone2.jpg",
+    imageUrl: "/products/05-headphone2.jpg",
   },
   {
     name: "Joystick",
     description: "High performance joystick",
-    imageUrl: "/assets/products07-joystick.jpg",
+    imageUrl: "/products/07-joystick.jpg",
   },
   {
     name: "Earpods",
     description: "Premium quality earpods",
-    imageUrl: "/assets/products09-earpod.jpg",
+    imageUrl: "/products/09-earpod.jpg",
   },
   {
     name: "Watch",
     description: "Premium white color watch",
-    imageUrl: "/assets/products02-watch.jpg",
+    imageUrl: "/products/02-watch.jpg",
   },
   {
     name: "Watch",
     description: "Standard black watch",
-    imageUrl: "/assets/products04-watch2.jpg",
+    imageUrl: "/products/04-watch2.jpg",
   },
   {
     name: "DSLR camera",
     description: "DSLR camera",
-    imageUrl: "/assets/products06-camera.jpg",
+    imageUrl: "/products/06-camera.jpg",
   },
   {
     name: "Backpack",
     description: "Comfortable backpack",
-    imageUrl: "/assets/products08-backpack.jpg",
+    imageUrl: "/products/08-backpack.jpg",
   },
   {
     name: "Mechanical keyboard",
     description: "High quality premium, ultra comfortable mechanical keyboard",
-    imageUrl: "/assets/products10-mechanical-keyboard.jpg",
+    imageUrl: "/products/10-mechanical-keyboard.jpg",
   },
 ];
 
