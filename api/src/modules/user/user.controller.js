b25afb9ee49c70199cb9c2bb3b70eceef4b80ee8
Raziur Rahman <instrumaniak@gmail.com>
2022-02-11 12:17:18 +0600

    be: user login api

--- api/src/modules/user/user.controller.js
+++ api/src/modules/user/user.controller.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../../config");
 const UserModel = require("./user.model");
 
 exports.register = (req, res, next) => {
@@ -38,8 +40,52 @@ exports.register = (req, res, next) => {
   });
 };
 
-exports.login = (req, res) => {
-  res.json({
-    msg: "login",
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  UserModel.findOne({ email }).exec((err, user) => {
+    if (err) return next(err);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: "User not found",
+      });
+    }
+
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) return next(err);
+
+      if (isMatch) {
+        const userData = {
+          userId: user._id,
+          name: user.name,
+          email: user.email,
+          phone: user.phone,
+        };
+        const token = jwt.sign(
+          {
+            type: "user",
+            data: userData,
+          },
+          config.JWT_SECRET,
+          {
+            expiresIn: config.JWT_EXPIRES_IN,
+          }
+        );
+
+        return res.json({
+          success: true,
+          access_token: "JWT " + token,
+          data: userData,
+        });
+      } else {
+        return res.json({
+          success: false,
+          param: "password",
+          msg: "Wrong password",
+        });
+      }
+    });
   });
 };
